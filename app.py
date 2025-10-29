aac8fed7661bf85ba1417059a8258628869d35dc
Ashish Kamble <ashishkamble0422@gmail.com>
2025-10-30 01:05:02 +0530

    app made accessible for all networks,dynamicportadded

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pickle
+import os
 
 import pandas as pd
 
@@ -48,4 +49,8 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
+
+
